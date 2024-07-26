117fdcace7ff42c7f8d5aa7622f1e697ba5c9bfc
Abduvohid-A <abduvohidabdurahimov01@gmail.com>
2024-07-27 02:54:26 +0500

    from another account

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import router from "./routes/index.routes.js";
-import { loggerMiddleware} from "./middlewares/logger.js";
+import { loggerMiddleware } from "./middlewares/logger.js";
 
 dotenv.config();
 
--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,8 +1,8 @@
-import { logger } from "../../logger.js";
+import { sillylogger } from "../../logger.js";
 
 export const loggerMiddleware = async (req, res, next) => {
     const log = `Request method: ${req.method} Request url: ${req.url}`;
-    logger.http(log);
+    sillylogger.http(log);
     next();
 };
 
